fbb1393008c3674c7eee363a9cac933bf3f0264f
Your Name <you@example.com>
2020-06-24 22:22:05 +0530

    First commit

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products:[
+        {
+        name: 'Slim Fit Shirt',
+        category:'Men',
+        image:'/images/d1.jpg',
+        price:1000,
+        brand: 'Embark',
+        rating: 5.3,
+        numReviews: 10
+    },
+    {
+        name: 'Shirt',
+        category:'Men',
+        image:'/images/d2.jpg',
+        price:1500,
+        brand: 'Embark',
+        rating: 5.3,
+        numReviews: 10
+    },
+    {
+        name: 'short',
+        category:'Men',
+        image:'/images/d3.jpg',
+        price:1400,
+        brand: 'Embark',
+        rating: 5.3,
+        numReviews: 10
+    },
+    {
+        name: 'Women tShirt',
+        category:'Women',
+        image:'/images/d4.jpg', 
+        price:1070,
+        brand: 'Embark',
+        rating: 5.3,
+        numReviews: 10
+    },
+    {
+        name: 'Women pants',
+        category:'Women',
+        image:'/images/d5.jpg',
+        price:1500,
+        brand: 'Embark',
+        rating: 5.3,
+        numReviews: 10
+    },
+]
+}
